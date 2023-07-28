bb5051fd8693c663d256d77cb702b457b9f489d9
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-07-28 15:59:41 -0500

    Board_SetGridSize

---
+++ BoardTests.cs
@@ -0,0 +1,26 @@
+﻿using Gomoku.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Gomoku.Models.Cell;
+
+namespace GomokuTests
+{
+    public class BoardTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Board_SetGridSize()
+        {
+            var board = new Board();
+            board.SetGridSize(10);
+            Assert.That(board.GetGridSize, Is.EqualTo(10));
+        }
+    }
+}
