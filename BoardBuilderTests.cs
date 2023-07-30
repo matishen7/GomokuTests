c028601c4cef54924aaf0ff0a48d3d85459fb68f
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-07-30 15:33:29 -0500

    BoardBuilderTests

---
+++ BoardBuilderTests.cs
@@ -0,0 +1,34 @@
+﻿using Gomoku.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GomokuTests
+{
+    public class BoardBuilderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BoardBuilder_SetGridSize()
+        {
+            var bd = new BoardBuilder()
+                .WithGridSize(10)
+                .Build();
+            Assert.That(bd.GetGridSize, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void BoardBuilder_DefaultGridSize()
+        {
+            var bd = new BoardBuilder()
+                .Build();
+            Assert.That(bd.GetGridSize, Is.EqualTo(15));
+        }
+    }
+}
