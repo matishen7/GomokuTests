08cbd0733830d3e2a9243d583a4390804c9c9d53
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-07-30 15:59:31 -0500

    ComputerPlayer_When_Move_Success

--- ComputerPlayerTests.cs
+++ ComputerPlayerTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,7 @@ namespace GomokuTests
         }
 
         [Test]
-        public void BoardBuilder_SetGridSize()
+        public void ComputerPlayer_When_Null_Board()
         {
             var bd = new BoardBuilder()
                 .WithGridSize(10)
@@ -27,6 +28,25 @@ namespace GomokuTests
             var cp = new ComputerPlayer();
             Assert.That(cp.GetName(), Is.Not.Null);
             Assert.That(() => cp.Move(null, 0, 0), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void ComputerPlayer_When_Move_Success()
+        {
+            var bd = new BoardBuilder()
+                .WithGridSize(10)
+                .Build();
+            Assert.That(bd.GetGridSize, Is.EqualTo(10));
+
+            var cp = new ComputerPlayer();
+            bd = cp.Move(bd, 0, 0);
+            int count = 0;
+            for(var i  = 0;i < bd.GetGridSize();i++)
+                for (var j = 0; j < bd.Grid[i].Length;j++)
+                    if (bd.Grid[i][j].Color == StoneColor.White)
+                        count++;
+
+            Assert.That(count, Is.EqualTo(1));
         }
     }
 }
